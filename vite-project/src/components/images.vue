9947a0e53aab1322616af1a047f230f7f0f501c7
Ainhoa <157162949+JVPpolitecnics@users.noreply.github.com>
2024-05-22 19:01:06 +0200

    dist

---
+++ vite-project/src/components/images.vue
@@ -0,0 +1,19 @@
+<template>
+    <img src="/restaurant/Restaurant.gif" alt="Restaurant Main Image">
+<img src="/foodLink/Group.png" alt="FoodLink Main Image">
+<img src="/game/game.gif" alt="Game Main Image">
+<img src="/restaurant/restaurantLogo.png" alt="Restaurant Logo">
+<img src="/foodLink/FoodLinkLOGO.png" alt="FoodLink Logo">
+<img src="/game/logoCientifiks.png" alt="Game Logo">
+<img src="/restaurant/za.gif" alt="Restaurant Highlight Image">
+<img src="" alt="No Highlight Image Available">
+<img src="/game/gameHighlight.gif" alt="Game Highlight Image">
+
+
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
